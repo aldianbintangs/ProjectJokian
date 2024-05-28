4497a90cb21e79ff427fd6f8bb7e6548b3694601
Aldiansyah <abintangs26@gmail.com>
2024-05-29 00:18:51 +0700

    fixing error

--- back-end/resources/views/admin/dashboard.blade.php
+++ back-end/resources/views/admin/dashboard.blade.php
@@ -1,19 +1,33 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="{{ asset("/assets/css/admin_dashboard.css") }}">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap');
+    </style>
 </head>
-
 <body>
-    <h1>Welcome, Admin</h1>
-    <a href="{{ route('logout') }}"
-        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+    <div class="profile">
+        <p>Hi, Admin {{ Auth::user()->username }}</p>
+    </div>
+    <h1>ADMIN DASHBOARD</h1>
+    <div class="line"></div>
+    <div class="content">
+        <button><a href="{{ url("") }}">
+            <img src="{{ asset("/assets/image/calendar_icons.png") }}" alt="">
+            <p>TAMBAHKAN EVENT</p>
+        </a></button>
+        <button><a href="{{ route('admin.users.index') }}">
+            <img src="{{ asset("/assets/image/People_icons.png") }}" alt="">
+            <p>KELOLA USER</p>
+        </a></button>
+    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
         @csrf
     </form>
+    </div>
 </body>
-
 </html>
