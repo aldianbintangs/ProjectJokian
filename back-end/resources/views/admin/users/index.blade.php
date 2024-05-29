6a930658354a0dcf06dfbf0645333d63e35f9cab
adnanfaikar <adnan.faika@gmail.com>
2024-05-29 19:43:03 +0700

    adding new feature

--- back-end/resources/views/admin/users/index.blade.php
+++ back-end/resources/views/admin/users/index.blade.php
@@ -21,7 +21,7 @@
     <div class="container">
         <div class="user-management">
             <a href="/dashboard/admin"><img src="{{ asset('assets/image/back-button.png') }}" alt=""></a>
-            <button class="add-user-btn"> <a href="{{ route('admin.users.create') }}">Add User</a></button>
+            <button class="add-user-btn"> Add User</button>
             <table>
                 <thead>
                     <tr>
@@ -38,19 +38,68 @@
                             <td>{{ $user->username }}</td>
                             <td>{{ $user->email }}</td>
                             <td>
-                                <a href="{{ route('admin.users.edit', $user->id) }}" class="edit-btn">Edit</a>
-                                <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="delete-btn">Delete</button>
-                                </form>
+                                <div class="button">
+                                    <button class="edit-btn">
+                                        <a href="{{ route('admin.users.edit', $user->id) }}" class="link-btn">Edit</a>
+                                    </button>
+                                    <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="delete-btn link-btn">Delete</button>
+                                    </form>
+                                </div>
+
                             </td>
+
                         </tr>
                     @endforeach
                 </tbody>
             </table>
         </div>
     </div>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h2>Add New User</h2>
+            <form action="{{ route('admin.users.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <button type="submit" class="submit-btn">Submit</button>
+            </form>
+        </div>
+    </div>
+    <script>
+        var modal = document.getElementById("myModal");
+        var btn = document.querySelector(".add-user-btn");
+        var span = document.getElementsByClassName("close")[0];
+
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+
+
 </body>
 
 </html>
